29d0eec2b14ad8568d7f6b52d842642a2e9f686f
ISHIMWE Jean De Dieu <jeandedieuishimwe109@gmail.com>
2026-01-19 16:05:25 +0000

    have add the record txt to e verified in search console?

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/chat',
-        permanent: true,
-      },
-    ]
-  },
   typescript: {
     // This will prevent TypeScript errors from failing the build.
     ignoreBuildErrors: true,
