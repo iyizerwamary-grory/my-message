ee9b9ff1cdd9c39f8d0d8b587db88bd976477f05
ISHIMWE Jean De Dieu <jeandedieuishimwe109@gmail.com>
2026-01-19 15:11:42 +0000

    tell me free server that can store all story work with my social media

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   transpilePackages: ['@radix-ui/react-dialog'],
